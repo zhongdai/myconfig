9bae75ae03f3e17340f542768005a6dcd89622c5
Zhong Dai <zhongdai.au@gmail.com>
2018-11-21 09:26:18 +1100

    add git part to the setup.sh

--- link.sh
+++
@@ -1,2 +0,0 @@
-ln -s $(pwd)/.vimrc ~/.vimrc
-ln -s $(pwd)/.tmux.confi ~/.tmux.conf
---
+++ setup.sh
@@ -0,0 +1,12 @@
+
+# create alias for git command
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+# Link the config files to ~
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.tmux.confi ~/.tmux.conf
