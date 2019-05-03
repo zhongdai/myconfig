6ca74758212b7042d7d15a1e6db62a45df76ccec
Zhong Dai <zhdai@woolworths.com.au>
2019-05-03 19:44:25 +1000

    added zsh custom script

---
+++ config/myconfig.zsh
@@ -0,0 +1,9 @@
+# The custom file for zsh to override setting in .zshrc
+
+
+export LANG=en_US.UTF-8
+export UPDATE_ZSH_DAYS=30
+
+
+alias zshconfig="vi $HOME/.zshrc"
+alias ohmyzsh="vi $HOME/.oh-my-zsh"
